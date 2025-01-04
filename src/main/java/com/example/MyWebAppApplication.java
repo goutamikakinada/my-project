e576193fe55037c0001b3d0edc23240638221b40
ganni1 <ganni1@gmail.com>
2025-01-04 17:36:03 +0530

    updated-.java-1

--- src/main/java/com/example/MyWebAppApplication.java
+++ src/main/java/com/example/MyWebAppApplication.java
@@ -2,18 +2,11 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyWebAppApplication extends SpringBootServletInitializer {
+public class MyWebAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyWebAppApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(MyWebAppApplication.class);
     }
 }
